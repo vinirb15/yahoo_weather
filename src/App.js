41e90ff9def5b7d57c8c859399593048af50a10a
vbueno <viniciusbueno15@gmail.com>
2020-10-30 18:20:21 -0300

    first page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
 import './App.css';
 
+import Image from './assets/SVG/31.svg';
+
 function App() {
   return (
     <div className="App">
-      <h1></h1>
+
+      <div className="container">
+
+        <input type="text" />
+
+        <div className="today">
+          <img src={Image} alt="" srcset="" />
+          <div className="content">
+            <h1>hoje 23c</h1>
+            <h2>Parcialmente Nublado</h2>
+            <p>Vento: 9km/h</p>
+            <p>Humidade: 80%</p>
+            <p>Pressão: 1003hPA</p>
+          </div>
+        </div>
+
+        <div className="after-today">
+          <img src={Image} alt="" srcset="" />
+          <div className="content">
+            <h1>hoje 23c</h1>
+            <h2>Parcialmente Nublado</h2>
+            <p>Vento: 9km/h</p>
+            <p>Humidade: 80%</p>
+            <p>Pressão: 1003hPA</p>
+          </div>
+        </div>
+
+
+
+        <div className="after-today">
+          <img src={Image} alt="" srcset="" />
+          <div className="content">
+            <h1>hoje 23c</h1>
+            <h2>Parcialmente Nublado</h2>
+            <p>Vento: 9km/h</p>
+            <p>Humidade: 80%</p>
+            <p>Pressão: 1003hPA</p>
+          </div>
+        </div>
+
+
+      </div>
+
     </div>
   );
 }
