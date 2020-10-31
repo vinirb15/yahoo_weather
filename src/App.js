d5208c9f48e19f1e176c7f49bae88d8f42727487
vbueno <viniciusbueno15@gmail.com>
2020-10-31 18:35:04 -0300

    folder correction / axios usage

--- src/App.js
+++ src/App.js
@@ -1,54 +1,10 @@
-import './App.css';
+import React from 'react';
 
-import Image from './assets/SVG/31.svg';
+import Temp from './pages/index';
 
 function App() {
   return (
-    <div className="App">
-
-      <div className="container">
-
-        <input type="text" />
-
-        <div className="today">
-          <img src={Image} alt="" srcset="" />
-          <div className="content">
-            <h1>hoje 23c</h1>
-            <h2>Parcialmente Nublado</h2>
-            <p>Vento: 9km/h</p>
-            <p>Humidade: 80%</p>
-            <p>Pressão: 1003hPA</p>
-          </div>
-        </div>
-
-        <div className="after-today">
-          <img src={Image} alt="" srcset="" />
-          <div className="content">
-            <h1>hoje 23c</h1>
-            <h2>Parcialmente Nublado</h2>
-            <p>Vento: 9km/h</p>
-            <p>Humidade: 80%</p>
-            <p>Pressão: 1003hPA</p>
-          </div>
-        </div>
-
-
-
-        <div className="after-today">
-          <img src={Image} alt="" srcset="" />
-          <div className="content">
-            <h1>hoje 23c</h1>
-            <h2>Parcialmente Nublado</h2>
-            <p>Vento: 9km/h</p>
-            <p>Humidade: 80%</p>
-            <p>Pressão: 1003hPA</p>
-          </div>
-        </div>
-
-
-      </div>
-
-    </div>
+    <Temp />
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+
+import './styles.css';
+
+import bing from '../services/api'
+
+import Image from '../assets/SVG/31.svg';
+
+export default function Temp() {
+    const [background, setBackground] = useState([]);
+
+
+    useEffect(() => {
+        bing.get().then(response => {
+            setBackground(response.data.images[0].url);
+        })
+    }, []);
+
+    console.log(background)
+    return (
+        <div className="App">
+
+            <div className="container">
+
+                <input type="text" />
+
+                <div className="today">
+                    <img src={Image} alt="" />
+                    <div className="content">
+                        <h1>hoje 23c</h1>
+                        <h2>Parcialmente Nublado</h2>
+                        <p>Vento: 9km/h</p>
+                        <p>Humidade: 80%</p>
+                        <p>Pressão: 1003hPA</p>
+                    </div>
+                </div>
+
+                <div className="after-today">
+                    <img src={Image} alt="" />
+                    <div className="content">
+                        <h1>hoje 23c</h1>
+                        <h2>Parcialmente Nublado</h2>
+                        <p>Vento: 9km/h</p>
+                        <p>Humidade: 80%</p>
+                        <p>Pressão: 1003hPA</p>
+                    </div>
+                </div>
+
+
+
+                <div className="after-today">
+                    <img src={Image} alt="" />
+                    <div className="content">
+                        <h1>hoje 23c</h1>
+                        <h2>Parcialmente Nublado</h2>
+                        <p>Vento: 9km/h</p>
+                        <p>Humidade: 80%</p>
+                        <p>Pressão: 1003hPA</p>
+                    </div>
+                </div>
+
+
+            </div>
+
+        </div>
+    );
+}
