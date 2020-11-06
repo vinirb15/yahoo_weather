6de3a17338135662da1b9115d2a58a031032ea32
vbueno <viniciusbueno15@gmail.com>
2020-11-06 16:52:34 -0300

    Parser

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import './styles.css';
 
 import bing from '../services/bing';
 import Loader from '../components/loader';
+import { yw_ptbr, code_parser } from '../components/Parser';
 
 export default function Temperature() {
     const [actualTemperature, setActualTemperature] = useState({
@@ -38,113 +39,6 @@ export default function Temperature() {
     const [loading, setLoading] = useState(true)
 
     const [location, setLocation] = useState('')
-
-    var yw_ptbr = {
-        '0': 'tornado',
-        '1': 'tempestade tropical',
-        '2': 'furacão',
-        '3': 'tempestade severa',
-        '4': 'trovoadas',
-        '5': 'chuva e neve',
-        '6': 'chuva e granizo fino',
-        '7': 'neve e granizo fino',
-        '8': 'garoa gélida',
-        '9': 'garoa',
-        '10': 'chuva gélida',
-        '11': 'chuvisco',
-        '12': 'chuva',
-        '13': 'neve em flocos finos',
-        '14': 'leve precipitação de neve',
-        '15': 'ventos com neve',
-        '16': 'neve',
-        '17': 'chuva de granizo',
-        '18': 'pouco granizo',
-        '19': 'pó em suspensão',
-        '20': 'neblina',
-        '21': 'névoa seca',
-        '22': 'enfumaçado',
-        '23': 'vendaval',
-        '24': 'ventando',
-        '25': 'frio',
-        '26': 'nublado',
-        '27': 'muitas nuvens (noite)',
-        '28': 'muitas nuvens (dia)',
-        '29': 'parcialmente nublado (noite)',
-        '30': 'parcialmente nublado (dia)',
-        '31': 'céu limpo (noite)',
-        '32': 'ensolarado',
-        '33': 'tempo bom (noite)',
-        '34': 'tempo bom (dia)',
-        '35': 'chuva e granizo',
-        '36': 'quente',
-        '37': 'tempestades isoladas',
-        '38': 'tempestades esparsas',
-        '39': 'tempestades esparsas',
-        '40': 'chuvas esparsas',
-        '41': 'nevasca',
-        '42': 'tempestades de neve esparsas',
-        '43': 'nevasca',
-        '44': 'parcialmente nublado',
-        '45': 'chuva com trovoadas',
-        '46': 'tempestade de neve',
-        '47': 'relâmpagos e chuvas isoladas',
-        '3200': 'não disponível'
-    }
-
-    var code_parser = {
-        0: 45,
-        1: 13,
-        2: 13,
-        3: 6,
-        4: 16,
-        5: 23,
-        6: 23,
-        7: 23,
-        8: 20,
-        9: 17,
-        10: 20,
-        11: 17,
-        12: 18,
-        13: 24,
-        14: 24,
-        15: 21,
-        16: 23,
-        17: 20,
-        18: 20,
-        19: 20,
-        20: 25,
-        21: 13,
-        22: 25,
-        23: 6,
-        24: 6,
-        25: 6,
-        26: 25,
-        27: 9,
-        28: 8,
-        29: 9,
-        30: 8,
-        31: 3,
-        32: 2,
-        33: 3,
-        34: 2,
-        35: 24,
-        36: 2,
-        37: 26,
-        38: 26,
-        39: 26,
-        40: 17,
-        41: 21,
-        42: 21,
-        43: 21,
-        44: 14,
-        45: 26,
-        46: 22,
-        47: 15,
-        48: 45,
-        49: 45
-    }
-
-
 
     var url = 'https://weather-ydn-yql.media.yahoo.com/forecastrss';
     var method = 'GET';
