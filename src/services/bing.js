891a6d4d6d2df220c624ba92cb02f2f9b26fbf41
vbueno <viniciusbueno15@gmail.com>
2020-11-10 10:25:11 -0300

    api rust background

--- src/services/bing.js
+++ src/services/bing.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const bing = axios.create({
-    baseURL: 'https://cors-anywhere.herokuapp.com/https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR',
+    baseURL: 'http://localhost:8000/image',
 })
 
 export default bing;
